5e6ebf257d245e55ca30e28aa726087e22a07012
MajaMarkovic1 <markovicm9000@gmail.com>
2018-08-07 21:08:30 +0200

    zadatak6

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -8,6 +8,10 @@
             <input v-model="newAnimal.name" placeholder="Name" type="text"><br>
             <label>Date of Birth: </label>
             <input v-model="newAnimal.dateOfBirth" placeholder="dateOfBirth" type="text"><br>
+            <label>Sector: </label>
+           <select v-model="newAnimal.sector">
+                <option v-for="(sector, key) in sectors" :key="key" v-bind:value='sector'>{{sector.name}}</option><br>
+            </select><br>
             <button @click="addAnimal">Add animal</button>
         </form>
         <table>
@@ -15,6 +19,8 @@
                 <th>Type</th>
                 <th>Name</th>
                 <th>Date of Birth</th>
+                <th>Sector name</th>
+                <th>Sector space</th>                
                 <th></th>
                 <th></th>                
             </thead>
@@ -23,6 +29,8 @@
                     <td>{{ animal.type }}</td>
                     <td>{{ animal.name }}</td>
                     <td>{{ animal.dateOfBirth ? animal.dateOfBirth : "Unknown"  }}</td>
+                    <td>{{ animal.sector.name }}</td>
+                    <td>{{ animal.sector.space }}</td>
                     <button @click="removeAnimal(animal)">Remove</button>
                     <button @click="moveToTop(animal)">Move to top</button>
                 </tr>
@@ -32,19 +40,28 @@
 </template>
 
 <script>
+
+const sectors = [
+    {name: 'memos', space: 'cage'},
+    {name: 'reptiles', space: 'cage'},
+    {name: 'fish', space: 'water'}
+    
+]
+
 export default {
   name: 'AnimalList',
   data(){
       return {
           animals: [
-              {type: 'monkey', name: 'Mihajlo', dateOfBirth: '01/01/2010'},
-              {type: 'lion', name: 'Milan', dateOfBirth: '01/02/2011'},
-              {type: 'snake', name: 'Damir', dateOfBirth: '02/01/2012'},
-              {type: 'tiger', name: 'Maja', dateOfBirth: '03/02/2013'},
-              {type: 'elephant', name: 'Marko', dateOfBirth: ''}
+              {type: 'bear', name: 'Mihajlo', dateOfBirth: '01/01/2010', sector: sectors[0]},
+              {type: 'lion', name: 'Milan', dateOfBirth: '01/02/2011', sector: sectors[0]},
+              {type: 'snake', name: 'Damir', dateOfBirth: '02/01/2012', sector: sectors[1]},
+              {type: 'tiger', name: 'Maja', dateOfBirth: '03/02/2013', sector: sectors[0]},
+              {type: 'shark', name: 'Marko', dateOfBirth: '', sector: sectors[2]}
               
           ],
-          newAnimal: {}
+          newAnimal: {},
+          sectors: sectors
 
       }
   },
@@ -53,12 +70,15 @@ export default {
         let indexOfAnimal = this.animals.indexOf(animal);
         this.animals.splice(indexOfAnimal, 1);
     },
+
     moveToTop(animal){
         this.removeAnimal(animal);
         this.animals.unshift(animal);  
     },
+
     addAnimal(){
         this.animals.push(this.newAnimal);
+        this.newAnimal = {};
     }
      
   }
